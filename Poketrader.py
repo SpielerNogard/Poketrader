05f5f62f10068fa177f10c53a7cf80d3f97a8660
Christopher Müller <spielernogard@AirvonCistopher.fritz.box>
2021-07-22 07:28:19 +0200

    init

---
+++ Poketrader.py
@@ -0,0 +1,17 @@
+
+
+class Poketrader(object):
+    def __init__(self):
+        pass
+
+    def my_lists(self,name):
+        pass
+
+    def create_search(self,name):
+        pass
+
+    def create_offer(self,name):
+        pass
+
+    def give_all_pokemon(self):
+        pass
