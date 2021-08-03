85ea3841d75dc5e5df45369359dec17197a493a1
Christopher Müller <spielernogard@AirvonCistopher.fritz.box>
2021-08-03 18:20:29 +0200

    now uses SQLlite

--- Poketrader.py
+++ Poketrader.py
@@ -1,31 +1,51 @@
-from Databasewatcher import Database_Watcher
-
+import sqlite3 as sl
+con = sl.connect('Poketrader.db', check_same_thread=False)
 class Poketrader(object):
     def __init__(self):
-        self.KARL = Database_Watcher()
         pass
+
+    def read_data_from_table(self,SQL):
+        Ergebnis = []
+        with con:
+            data = con.execute(SQL)
+            for row in data:
+                print(row)
+                Ergebnis.append(row)
+        return(Ergebnis)
+
+    def insert_data_to_table(self,sql,data):
+        sql = 'INSERT INTO USER (id, name, age) values(?, ?, ?)'
+        data = [
+            (1, 'Alice', 21),
+            (2, 'Bob', 22),
+            (3, 'Chris', 23)
+        ]
+        with con:
+            con.executemany(sql, data)
 
     def my_lists(self,name):
         SQL_suchen = "SELECT * FROM suchen WHERE username = '"+str(name)+"';"
         SQL_anbieten = "SELECT * FROM anbieten WHERE username = '"+str(name)+"';"
-        Ergebnis = self.KARL.read_data(SQL_suchen)
-        Ergebnis_anbieten = self.KARL.read_data(SQL_anbieten)
-        print(Ergebnis, Ergebnis_anbieten)
+        Suchen = self.read_data_from_table(SQL_suchen)
+        Angebote = self.read_data_from_table(SQL_anbieten)
+        print(Suchen, Angebote)
 
     def create_search(self,username,pokemon_id,pokemon_name,is_shiny,ort,anzahl):
+        image = ""
         sql_for_image = "SELECT * from pokedex WHERE name = '"+str(pokemon_name)+"';"
-        SQL_suche_image = self.KARL.read_data(sql_for_image)
+        Ergebnis = self.read_data_from_table(sql_for_image)
         if is_shiny == 1:
-            image = SQL_suche_image[0]
+            image = Ergebnis[0]
             image = image[4]
         else:
-            image = SQL_suche_image[0]
+            image = Ergebnis[0]
             image = image[3]
         enthalten = False
         suche_anzahl = 0
         SQL_suche_sql = "SELECT * FROM suchen WHERE username = '"+str(username)+"';"
-        SQL_suche = self.KARL.read_data(SQL_suche_sql)
-        for suche in SQL_suche:
+        Ergebnis = self.read_data_from_table(SQL_suche_sql)
+        print(Ergebnis)
+        for suche in Ergebnis:
             print(suche)
             username_suche = suche[1]
             pokemon_name_suche = suche[3]
@@ -36,28 +56,33 @@ class Poketrader(object):
 
         if enthalten == False:
             SQL = "Insert into suchen(username, pokemon_id, pokemon_name, is_shiny,ort,anzahl,image) values('"+username+"',"+str(pokemon_id)+",'"+pokemon_name+"','"+str(is_shiny)+"','"+ort+"',"+str(anzahl)+",'"+image+"');"
-            self.KARL.write_data(SQL)
+            SQL2 = "Insert into suchen(username,pokemonid,pokemonname,isshiny,ort,anzahl,image) values(?,?,?,?,?,?,?);"
+            data = [username,pokemon_id,pokemon_name,is_shiny,ort,anzahl,image]
+            self.insert_data_to_table(SQL2,data)
         else:
             print("Ist bereits enthalten bitte updaten")
             print("Die neue Anzahl = ",str(suche_anzahl))
-            SQL_UPDATE = "UPDATE suchen SET anzahl = "+str(suche_anzahl)+" WHERE username = '"+str(username)+"' and pokemon_name = '"+str(pokemon_name)+"';"
-            self.KARL.write_data(SQL_UPDATE)
+            SQL_UPDATE = "UPDATE suchen SET anzahl = "+str(suche_anzahl)+" WHERE username = '"+str(username)+"' and pokemonname = '"+str(pokemon_name)+"';"
+            con.execute(SQL_UPDATE)
+            con.commit()
+        
 
     def create_offer(self,username,pokemon_id,pokemon_name,is_shiny,ort,anzahl):
+        image = ""
         sql_for_image = "SELECT * from pokedex WHERE name = '"+str(pokemon_name)+"';"
-        SQL_suche_image = self.KARL.read_data(sql_for_image)
+        Ergebnis = self.read_data_from_table(sql_for_image)
         if is_shiny == 1:
-            image = SQL_suche_image[0]
+            image = Ergebnis[0]
             image = image[4]
         else:
-            image = SQL_suche_image[0]
+            image = Ergebnis[0]
             image = image[3]
-
         enthalten = False
         suche_anzahl = 0
         SQL_suche_sql = "SELECT * FROM anbieten WHERE username = '"+str(username)+"';"
-        SQL_suche = self.KARL.read_data(SQL_suche_sql)
-        for suche in SQL_suche:
+        Ergebnis = self.read_data_from_table(SQL_suche_sql)
+        print(Ergebnis)
+        for suche in Ergebnis:
             print(suche)
             username_suche = suche[1]
             pokemon_name_suche = suche[3]
@@ -65,25 +90,29 @@ class Poketrader(object):
             if pokemon_name_suche == pokemon_name:
                 enthalten = True
                 suche_anzahl = suche_anzahl+anzahl
+
         if enthalten == False:
             SQL = "Insert into anbieten(username, pokemon_id, pokemon_name, is_shiny,ort,anzahl,image) values('"+username+"',"+str(pokemon_id)+",'"+pokemon_name+"','"+str(is_shiny)+"','"+ort+"',"+str(anzahl)+",'"+image+"');"
-            self.KARL.write_data(SQL)
+            SQL2 = "Insert into anbieten(username,pokemonid,pokemonname,isshiny,ort,anzahl,image) values(?,?,?,?,?,?,?);"
+            data = [username,pokemon_id,pokemon_name,is_shiny,ort,anzahl,image]
+            self.insert_data_to_table(SQL2,data)
         else:
             print("Ist bereits enthalten bitte updaten")
             print("Die neue Anzahl = ",str(suche_anzahl))
-            SQL_UPDATE = "UPDATE anbieten SET anzahl = "+str(suche_anzahl)+" WHERE username = '"+str(username)+"' and pokemon_name = '"+str(pokemon_name)+"';"
-            self.KARL.write_data(SQL_UPDATE)
+            SQL_UPDATE = "UPDATE anbieten SET anzahl = "+str(suche_anzahl)+" WHERE username = '"+str(username)+"' and pokemonname = '"+str(pokemon_name)+"';"
+            con.execute(SQL_UPDATE)
+            con.commit()
 
     def give_all_pokemon(self):
-        SQL_suche_sql = "SELECT * FROM pokedex;"
-        SQL_suche = self.KARL.read_data(SQL_suche_sql)
-        print(SQL_suche)
+        sql_for_image = "SELECT * from pokedex;"
+        Ergebnis = self.read_data_from_table(sql_for_image)
+        print(Ergebnis)
 
     def give_random_searchs_and_offers(self):
-        SQL_SEARCH = "SELECT * FROM suchen ORDER BY RAND() LIMIT 10;"
-        SQL_OFFERS = "SELECT * FROM anbieten ORDER BY RAND() LIMIT 10;"
-        searches = self.KARL.read_data(SQL_SEARCH)
-        offers = self.KARL.read_data(SQL_OFFERS)
+        SQL_SEARCH = "SELECT * FROM suchen ORDER BY RANDOM() LIMIT 10;"
+        SQL_OFFERS = "SELECT * FROM anbieten ORDER BY RANDOM() LIMIT 10;"
+        searches = self.read_data_from_table(SQL_SEARCH)
+        offers = self.read_data_from_table(SQL_OFFERS)
         Ergebnis = [searches,offers]
         return(Ergebnis)
 
@@ -91,13 +120,32 @@ class Poketrader(object):
         profilepicture = "https://www.senertec.de/wp-content/uploads/2020/04/blank-profile-picture-973460_1280.png"
         friendcode = "0000 0000 0000 0000"
         actual_place = "something"
-        SQL = "Insert into user_data(user_name, profile_picture, friend_code, actual_place) values('"+name+"','"+profilepicture+"','"+friendcode+"','"+actual_place+"');"
-        self.KARL.write_data(SQL)
+        SQL2 = "Insert into UserData(username, profilepicture, friendcode, actualplace) values(?,?,?,?);"
+        data = [name,profilepicture,friendcode,actual_place]
+        self.insert_data_to_table(SQL2,data)
+
+    def find_the_user(self,name):
+        SQL = "SELECT * FROM UserData WHERE username = '"+str(name)+"';"
+        offers = self.read_data_from_table(SQL)
+        this_user = offers[0]
+        return(this_user)
+    
+    def update_user(self,name, code, bild, ort):
+        SQL_UPDATE = "UPDATE UserData SET profilepicture = '"+str(bild)+"', friendcode='"+str(code)+"',actualplace='"+str(ort)+"' WHERE username = '"+str(name)+"';"
+        con.execute(SQL_UPDATE)
+        con.commit()
+        SQL_update_anbieten = "UPDATE anbieten SET ort = '"+str(ort)+"' WHERE username = '"+str(name)+"';"
+        con.execute(SQL_update_anbieten)
+        con.commit()
+        SQL_update_anbieten = "UPDATE suchen SET ort = '"+str(ort)+"' WHERE username = '"+str(name)+"';"
+        con.execute(SQL_update_anbieten)
+        con.commit()
 
 if __name__ == "__main__":
     BOB = Poketrader()
-    #BOB.my_lists("Vischie")
-    BOB.create_search("Vischie",1,"Partyhut Bisasam",1,"Köthen",1)
-    #BOB.create_offer("Vischie",1,"Bisasam",1,"Köthen",1)
     BOB.my_lists("Vischie")
+    #BOB.create_search("Vischie",1,"Partyhut Bisasam",1,"Köthen",1)
+    #BOB.create_offer("Vischie",1,"Bisasam",1,"Köthen",1)
+    #BOB.my_lists("Vischie")
+    #BOB.give_all_pokemon()
     BOB.give_all_pokemon()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 ######################          Import packages      ###################################
 ########################################################################################
 from Poketrader import Poketrader
-from flask import Blueprint, render_template, flash ,url_for
+from flask import Blueprint, render_template, flash ,url_for, request
 from flask_login import login_required, current_user
 from __init__ import create_app, db, BOB
 
@@ -19,15 +19,27 @@ def index():
     #return render_template('index.html')
     return(render_template('index.html',suchen = searches, angebote = offers)) 
     
-@main.route('/profile') # profile page that return 'profile'
+@main.route('/profile', methods=['GET', 'POST']) # profile page that return 'profile'
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name)
+    name = current_user.name
+    my_user = BOB.find_the_user(name)
+    print(my_user)
+    if request.method=='GET': # If the request is GET we return the sign up page and forms
+        return render_template('profile.html', name=current_user.name, user_data = my_user)
+    else:
+        code = request.form.get('code')
+        profilepic = request.form.get('profilepic')
+        orte = request.form.get('orte')
+        print(code,profilepic,orte)
+        BOB.update_user(current_user.name,code,profilepic,orte)
+        my_user = BOB.find_the_user(name)
+        return render_template('profile.html', name=current_user.name, user_data = my_user)
 
 @main.route('/trades') # The trade page for the User'
 @login_required
 def trades():
-    return render_template('profile.html', name=current_user.name)
+    return render_template('trades.html', name=current_user.name)
 
 app = create_app() # we initialize our flask app using the __init__.py function
 if __name__ == '__main__':
